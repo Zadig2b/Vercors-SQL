c5b839cad9d56ce337df492a07a9954c54beb4c5
=Justine <justine.lequellenec.simplon@gmail.com>
2024-03-26 14:15:02 +0100

    models added

--- src/migrations/vercors.sql
+++ src/migrations/vercors.sql
@@ -1,88 +1,77 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 26 mars 2024 à 11:58
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
+CREATE TABLE vercors_User(
+   Id_User INT AUTO_INCREMENT,
+   name VARCHAR(100)  NOT NULL,
+   surname VARCHAR(100)  NOT NULL,
+   phone INT NOT NULL,
+   address VARCHAR(200)  NOT NULL,
+   email VARCHAR(200)  NOT NULL,
+   password VARCHAR(8)  NOT NULL,
+   RGPD BOOLEAN NOT NULL,
+   PRIMARY KEY(Id_User)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE vercors_reservation(
+   Id_reservation INT AUTO_INCREMENT,
+   number_of_places INT NOT NULL,
+   is_discounted BOOLEAN NOT NULL,
+   total_price INT NOT NULL,
+   Id_User INT NOT NULL,
+   PRIMARY KEY(Id_reservation),
+   FOREIGN KEY(Id_User) REFERENCES vercors_User(Id_User)
+);
 
+CREATE TABLE vercors_options(
+   Id_options INT AUTO_INCREMENT,
+   name VARCHAR(200)  NOT NULL,
+   stock INT NOT NULL,
+   price INT,
+   PRIMARY KEY(Id_options)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE vercors_tent(
+   Id_tent INT AUTO_INCREMENT,
+   price INT,
+   PRIMARY KEY(Id_tent)
+);
 
---
--- Base de données : `vercors`
---
+CREATE TABLE vercors_van(
+   Id_van INT AUTO_INCREMENT,
+   price INT,
+   PRIMARY KEY(Id_van)
+);
 
--- --------------------------------------------------------
+CREATE TABLE pass(
+   Id_pass INT AUTO_INCREMENT,
+   type DATE,
+   price INT,
+   PRIMARY KEY(Id_pass)
+);
 
---
--- Structure de la table `customer`
---
+CREATE TABLE contains(
+   Id_reservation INT,
+   Id_options INT,
+   quantity INT NOT NULL,
+   PRIMARY KEY(Id_reservation, Id_options),
+   FOREIGN KEY(Id_reservation) REFERENCES vercors_reservation(Id_reservation),
+   FOREIGN KEY(Id_options) REFERENCES vercors_options(Id_options)
+);
 
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
-  `customer_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  PRIMARY KEY (`customer_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE date_pass(
+   Id_reservation INT,
+   Id_pass INT,
+   _date DATE,
+   PRIMARY KEY(Id_reservation, Id_pass),
+   FOREIGN KEY(Id_reservation) REFERENCES vercors_reservation(Id_reservation),
+   FOREIGN KEY(Id_pass) REFERENCES pass(Id_pass)
+);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `reservation`
---
-
-DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `reservation_id` int NOT NULL AUTO_INCREMENT,
-  `number_of_places` int NOT NULL,
-  `is_discounted` tinyint(1) NOT NULL,
-  `pass_type` varchar(20) NOT NULL,
-  `chosen_dates` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`reservation_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `reservationoption`
---
-
-DROP TABLE IF EXISTS `reservationoption`;
-CREATE TABLE IF NOT EXISTS `reservationoption` (
-  `option_id` int NOT NULL AUTO_INCREMENT,
-  `option_name` varchar(100) NOT NULL,
-  `option_price` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`option_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `reservation_option`
---
-
-DROP TABLE IF EXISTS `reservation_option`;
-CREATE TABLE IF NOT EXISTS `reservation_option` (
-  `reservation_id` int NOT NULL,
-  `option_id` int NOT NULL,
-  PRIMARY KEY (`reservation_id`,`option_id`),
-  KEY `option_id` (`option_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE accommodation(
+   Id_reservation INT,
+   Id_tent INT,
+   Id_van INT,
+   chosen_dates INT,
+   PRIMARY KEY(Id_reservation, Id_tent, Id_van),
+   FOREIGN KEY(Id_reservation) REFERENCES vercors_reservation(Id_reservation),
+   FOREIGN KEY(Id_tent) REFERENCES vercors_tent(Id_tent),
+   FOREIGN KEY(Id_van) REFERENCES vercors_van(Id_van)
+);
