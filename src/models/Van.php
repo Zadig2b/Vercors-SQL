e319d232bd5f334298d5e532dc1d362ae6c0bf17
=Justine <justine.lequellenec.simplon@gmail.com>
2024-03-28 09:45:50 +0100

    all models created with get and set

---
+++ src/models/Van.php
@@ -0,0 +1,48 @@
+<?php
+
+class Van {
+    private $Id_van;
+    private $price;
+
+    public function __construct($Id_van, $price) {
+        $this->Id_van = $Id_van;
+        $this->price = $price;
+
+    }
+
+    /**
+     * Get the value of Id_van
+     */
+    public function getIdVan()
+    {
+        return $this->Id_van;
+    }
+
+    /**
+     * Set the value of Id_van
+     */
+    public function setIdVan($Id_van): self
+    {
+        $this->Id_van = $Id_van;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     */
+    public function setPrice($price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
